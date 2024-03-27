d3c6e219bf170f38121df2940cfa45b8cf1daa6a
Stefan Liscak <liscak@gmail.com>
2024-03-27 11:40:21 +0100

    Adding .h5 data and .csv data. Adding codes for Structure factors

---
+++ Correlations.jl
@@ -0,0 +1,34 @@
+using ITensors, Printf, PyPlot, ITensors.HDF5
+include("functions.jl")
+
+let 
+
+    combinations = [("Sx", "Sx", "Sxx"), ("Sx", "Sy", "Sxy"), ("Sx", "Sz", "Sxz"), ("Sy", "Sx", "Syx"), ("Sy", "Sy", "Syy"), ("Sy", "Sz", "Syz"), ("Sz", "Sx", "Szx"), ("Sz", "Sy", "Szy"), ("Sz", "Sz", "Szz")]  
+
+    f = h5open("good_results_30/0_05_Mag2D_original.h5","r") 
+    ψ₁ = read(f,"Psi_1",MPS)
+    close(f)
+
+    f = h5open("good_results_30/0_05_Mag2D_conjugated.h5","r") 
+    ψ₂ = read(f,"Psi_2",MPS)
+    close(f)
+
+    c₁ = 1/sqrt(2)
+    ϕ = 7*pi/4
+    Ψ = c₁ * exp(-im * ϕ) * ψ₁ + sqrt(1 - c₁^2) * ψ₂
+
+    for (op1, op2, plot_title) in combinations
+
+        qx_mesh, qy_mesh, S_values = calculate_StructureFactor(Ψ, 1.0, 0.02, op1, op2)
+
+        figure()
+        pcolor(qx_mesh, qy_mesh, S_values, shading="auto")
+        colorbar()  
+
+        xlabel("q_x")
+        ylabel("q_y")
+        title(plot_title)
+        savefig("$(plot_title).jpg", dpi = 300)
+    end    
+
+end
---
+++ Superpositions.jl
@@ -0,0 +1,54 @@
+using ITensors, Printf, PyPlot, ITensors.HDF5
+pygui(true)
+include("functions.jl")
+
+
+let 
+
+    f = h5open("good_results_30/0_01_Mag2D_original.h5","r") #0.05
+    ψ₁ = read(f,"Psi_1",MPS)
+    close(f)
+
+    f = h5open("good_results_30/0_01_Mag2D_conjugated.h5","r") 
+    ψ₂ = read(f,"Psi_2",MPS)
+    close(f)
+
+    c₁ = 1/sqrt(2)
+    ϕ = 7*pi/4
+
+    Ψ = c₁ * exp(-im * ϕ) * ψ₁ + sqrt(1 - c₁^2) * ψ₂
+    
+    Mx = expect(Ψ,"Sx")
+    My = expect(Ψ,"Sy")
+    Mz = expect(Ψ,"Sz")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+
+    f = open("magnetisation.csv", "w")
+    for (j,mz) in enumerate(Mz)
+        L = sqrt(length(Mz))
+        t = Mx
+        x, y, z = (j-1.0) ÷ L , (j-1.0) % L , 0.0
+        cmap = PyPlot.matplotlib.cm.get_cmap("rainbow_r") 
+        vmin = minimum(t)
+        vmax = maximum(t)
+        norm = PyPlot.matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+        ax.quiver(x, y, z, Mx[j], My[j], Mz[j], normalize=true, color=cmap(norm(t[j])))
+
+        @printf f "%f,"  x
+        @printf f "%f,"  y
+        @printf f "%f,"  0.0
+        @printf f "%f,"  Mx[j]
+        @printf f "%f,"  My[j]
+        @printf f "%f,"  Mz[j]
+        @printf f "%f\n" sqrt(Mx[j]^2 + My[j]^2 + Mz[j]^2)
+    end
+    ax.set_aspect("equal")
+    plt.show()
+    close(f)
+
+    Q = calculate_TopoCharge_FromMag(Mx, My, Mz)
+    println(Q)
+    
+end
