3231396d166ba39fdd0cb98ad3cd35ae6d647c3b
steli42 <liscak@gmail.com>
2024-05-01 21:03:47 +0200

    cleaning up

--- kd_tree_approach /lattice.jl
+++ kd_tree_approach /lattice.jl
@@ -34,54 +34,7 @@ function epsilon(i, j, k)
     end
 end
 
-let
-    # construct quantum and classical lattice sites
-    L = 15
-    geom = "rectangular"
-    lattice_Q = build_lattice(L, L, geom)
-    lattice_C = build_lattice(L+2, L+2, geom)
-
-    idxs_QC = []
-    idxs_QH = []
-    for lQ in axes(lattice_Q, 2)
-        if lattice_Q[:,lQ] == [0.0, 0.0]
-            push!(idxs_QH, lQ)
-        end
-    end
-    lattice_Q = lattice_Q[:, setdiff(1:size(lattice_Q,2),idxs_QH)]
-    for lC in axes(lattice_C, 2)
-        for lQ in axes(lattice_Q, 2)
-            if lattice_C[:, lC] == lattice_Q[:, lQ] 
-                push!(idxs_QC, lC)
-            end
-        end
-    end
-    lattice_C = lattice_C[:, setdiff(1:size(lattice_C,2),idxs_QC)]
-
-    # construct tree objects for NearestNeighbors.jl
-    tree_Q = KDTree(lattice_Q, reorder=false)  
-    tree_C = KDTree(lattice_C, reorder=false)  
-
-    onsite_idxs = inrange(tree_Q, tree_Q.data, 0.01)  # return list of onsite indices
-    nn_idxs = inrange(tree_Q, tree_Q.data, 1.01)  # return list of onsite and nearest neighbors indices
-    nn_idxs_QQ = setdiff.(nn_idxs, onsite_idxs)  # subtract onsite indices so that only legit nearest-neighbors remain
-    nn_idxs_QC = inrange(tree_Q, tree_C.data, 1.01) # return list of nearest neighbors between classical and quantum sites
-    
-    # plt.scatter(lattice_Q[1,:], lattice_Q[2,:])
-    # plt.scatter(lattice_C[1,:], lattice_C[2,:])
-    # for id in axes(lattice_Q, 2)
-    #     plt.text(lattice_Q[1,id], lattice_Q[2,id], "$id")
-    # end
-    # for id in axes(lattice_C, 2)
-    #     plt.text(lattice_C[1,id], lattice_C[2,id], "$id")
-    # end
-    # plt.show()
-
-    nsweeps = 100
-    maxdim = [20 for n = 1:nsweeps]
-    cutoff = 1e-10
-    obs = DMRGObserver(; energy_tol = 1e-7, minsweeps = 10)
-
+function fetch_initial_state(lattice_Q::Array{Float64,2}, Lx::Int64, Ly::Int64)
     sites = siteinds("S=1/2", size(lattice_Q, 2))
     states = ["Up" for i = 1:size(lattice_Q, 2)]
     ψ₀ = MPS(sites,states)
@@ -90,20 +43,24 @@ let
         x,y = lattice_Q[1,idx],lattice_Q[2,idx]
         r = sqrt(x^2 + y^2)
         f = atan(y,x) + π/2
-        t = 1.9*π/2*(1.0-r)/sqrt(0.25*L^2 + 0.25*L^2)
+        t = 1.9*π/2*(1.0-r)/sqrt(0.25*Lx^2 + 0.25*Ly^2)
 
         Ryn = exp(-im*t*op("Sy", siteinds(ψ₀), idx))
         Rzn = exp(-im*f*op("Sz", siteinds(ψ₀), idx))
         ψ₀[idx] = Rzn*(Ryn*ψ₀[idx])
-    end    
+    end  
+
+    return ψ₀, sites
+end
+
+function build_hamiltonian(sites::Vector{Index{Int64}}, lattice_Q::Array{Float64,2}, lattice_C::Array{Float64,2},
+        nn_idxs_QQ::Vector{Vector{Int}}, nn_idxs_QC::Vector{Vector{Int}}, Bcr::Float64, J::Float64, D::Float64)
 
     Sv = ["Sx", "Sy", "Sz"]
     e_z = [0.0, 0.0, 1.0] #can serve as magnetisation vector for spins UP/DOWN -- m = ±1/2*e_z
-    J = -1.0
-    D = 2*π/L
-    Bcr = 0.5*D^2
     B = [0.0, 0.0, -0.55*Bcr]
 
+    ampo = 0.0
     ampo = OpSum()
     for idx in axes(lattice_Q, 2)
 
@@ -153,8 +110,72 @@ let
                 ampo += 0.5*D_vector[a]*epsilon(a,b,c)*e_z[c], Sv[b], nn_idx
             end
         end 
-    end    
+    end  
+
     H = MPO(ampo, sites)
+    return H
+end   
+
+#notes: why is the skyrmion always shifted along the [1,-1]-direction?
+# how can I get the bloch skyrmion instead of the neel skyrmion?
+
+let
+
+    nsweeps = 100
+    maxdim = [20 for n = 1:nsweeps]
+    cutoff = 1e-10
+    obs = DMRGObserver(; energy_tol = 1e-7, minsweeps = 10)
+    
+    Lx = 15
+    Ly = 15
+    J = -1.0
+    D = 2*π/sqrt(Lx*Ly)
+    Bcr = 0.5*D^2
+
+    # construct quantum and classical lattice sites
+    geom = "rectangular"
+    lattice_Q = build_lattice(Lx, Ly, geom)
+    lattice_C = build_lattice(Lx+2, Ly+2, geom)
+
+    idxs_QC = []
+    idxs_QH = []
+    for lQ in axes(lattice_Q, 2)
+        if lattice_Q[:,lQ] == [0.0, 0.0]
+            push!(idxs_QH, lQ)
+        end
+    end
+    lattice_Q = lattice_Q[:, setdiff(1:size(lattice_Q,2),idxs_QH)]
+    for lC in axes(lattice_C, 2)
+        for lQ in axes(lattice_Q, 2)
+            if lattice_C[:, lC] == lattice_Q[:, lQ] 
+                push!(idxs_QC, lC)
+            end
+        end
+    end
+    lattice_C = lattice_C[:, setdiff(1:size(lattice_C,2),idxs_QC)]
+
+    # construct tree objects for NearestNeighbors.jl
+    tree_Q = KDTree(lattice_Q, reorder=false)  
+    tree_C = KDTree(lattice_C, reorder=false)  
+
+    onsite_idxs = inrange(tree_Q, tree_Q.data, 0.01)  # return list of onsite indices
+    nn_idxs = inrange(tree_Q, tree_Q.data, 1.01)  # return list of onsite and nearest neighbors indices
+    nn_idxs_QQ = setdiff.(nn_idxs, onsite_idxs)  # subtract onsite indices so that only legit nearest-neighbors remain
+    nn_idxs_QC = inrange(tree_Q, tree_C.data, 1.01) # return list of nearest neighbors between classical and quantum sites
+    
+    # plt.scatter(lattice_Q[1,:], lattice_Q[2,:])
+    # plt.scatter(lattice_C[1,:], lattice_C[2,:])
+    # for id in axes(lattice_Q, 2)
+    #     plt.text(lattice_Q[1,id], lattice_Q[2,id], "$id")
+    # end
+    # for id in axes(lattice_C, 2)
+    #     plt.text(lattice_C[1,id], lattice_C[2,id], "$id")
+    # end
+    # plt.show()
+
+    ψ₀, sites = fetch_initial_state(lattice_Q, Lx, Ly)
+
+    H = build_hamiltonian(sites, lattice_Q, lattice_C, nn_idxs_QQ, nn_idxs_QC, Bcr, J, D)
 
     E, ψ = dmrg(H, ψ₀; nsweeps, maxdim, cutoff, observer = obs)
 
@@ -187,7 +208,6 @@ let
     #     plt.text(lattice_Q[1,id], lattice_Q[2,id], "$id")
     # end
     # plt.show()
-
 
     # ITensors.disable_warn_order()
     # full_H = 1.0
