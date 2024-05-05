ed8d20eea11f5f683d96c44ae5137ca9c65d6863
steli42 <liscak@gmail.com>
2024-05-05 10:36:27 +0200

    ...

--- kd_tree_approach/lattice.jl
+++ kd_tree_approach/lattice.jl
@@ -1,4 +1,5 @@
-using NearestNeighbors, Statistics, PyPlot, ITensors, Random, LinearAlgebra, SparseArrays
+using NearestNeighbors, Statistics, PyPlot, ITensors, Printf, LinearAlgebra, SparseArrays
+using HDF5
 pygui(true)
 
 function build_lattice(Lx::Int64, Ly::Int64, geometry::String)  # construct lattice sites 
@@ -186,12 +187,31 @@ let
     maxdim = [20 for n = 1:nsweeps]
     cutoff = 1e-10
     obs = DMRGObserver(; energy_tol = 1e-7, minsweeps = 10)
-    
-    Lx, Ly = 15, 15
+    isAdiabatic = true
+
+    δ = 0.02
+    Δ = 0.1
+    Lx, Ly = 11, 11
     J = -1.0
     D = π/sqrt(Lx*Ly)
     Bcr = 0.5*D^2
 
+    α_range₁ = 1.0:-Δ:0.2
+    α_range₂ = 0.2:-δ:0.0
+    α_values_pos = unique(collect(Iterators.flatten((α_range₁,α_range₂))))
+    α_values_neg = sort(map(x -> -x, α_values_pos))
+    α = 1.0
+ 
+    Energies = []
+
+    # Define directories
+    original_dir = "original"
+    conjugated_dir = "conjugated"
+
+    # Create directories if they don't exist
+    isdir(original_dir) || mkdir(original_dir)
+    isdir(conjugated_dir) || mkdir(conjugated_dir)
+    
     # construct quantum and classical lattice sites
     geom = "rectangular"  #at this point triangular does not work
     lattice_QH = build_lattice(Lx, Ly, geom)
@@ -238,6 +258,11 @@ let
     H = build_hamiltonian(sites, lattice_Q, lattice_C, nn_idxs_QQ, nn_idxs_QC, Bcr, J, D)
 
     E, ψ = dmrg(H, ψ₀; nsweeps, maxdim, cutoff, observer = obs)
+    σ = inner(H,ψ,H,ψ) - E^2
+    
+    if isAdiabatic
+        ψ₀ = ψ
+    end
 
     sx_expval = expect(ψ, "Sx")
     sy_expval = expect(ψ, "Sy")
@@ -247,6 +272,24 @@ let
     if origin_index !== nothing
         insert_magnetization!(sx_expval, sy_expval, sz_expval, origin_index, [0.0, 0.0, 0.5])
     end
+
+    formatted_alpha = replace(string(round(α, digits=2)), "." => "_")
+    original_file_path = joinpath(original_dir, "$(formatted_alpha)_Mag2D_original.csv")
+    conjugated_file_path = joinpath(conjugated_dir, "$(formatted_alpha)_Mag2D_conjugated.csv")
+    
+    f_original = open(original_file_path, "w")
+    for idx in axes(lattice_QH,2)
+      @printf f_original "%f,"  lattice_QH[1,idx]
+      @printf f_original "%f,"  lattice_QH[2,idx]
+      @printf f_original "%f,"  0.0
+      @printf f_original "%f,"  sx_expval[idx]
+      @printf f_original "%f,"  sy_expval[idx]
+      @printf f_original "%f,"  sz_expval[idx]
+      @printf f_original "%f\n" sqrt(sx_expval[idx]^2 + sy_expval[idx]^2 + sz_expval[idx]^2)
+    end  
+    close(f_original)
+    println("For alpha = $α: Final energy of psi = $E")
+    println("For alpha = $α: Final energy variance of psi = $σ")
 
     fig = plt.figure()
     ax = fig.add_subplot(projection = "3d")
@@ -268,6 +311,67 @@ let
     Q = calculate_TopoCharge(sx_expval, sy_expval, sz_expval, lattice_QH, Lx, Ly)
     @show Q
 
+    ###################################################################################
+    ψ_c = conj.(ψ)
+    E_c = inner(ψ_c', H, ψ_c)
+    σ_c = inner(H, ψ_c, H, ψ_c) - E_c^2
+
+    sx_expval_c = expect(ψ_c, "Sx")
+    sy_expval_c = expect(ψ_c, "Sy")
+    sz_expval_c = expect(ψ_c, "Sz")
+
+    origin_index = findfirst(isequal([0.0, 0.0]), eachcol(lattice_QH)) # finds the index of point [0,0]
+    if origin_index !== nothing
+        insert_magnetization!(sx_expval_c, sy_expval_c, sz_expval_c, origin_index, [0.0, 0.0, 0.5])
+    end
+
+    f_conjugated = open(conjugated_file_path, "w")
+    for idx in axes(lattice_QH,2)
+      @printf f_conjugated "%f,"  lattice_QH[1,idx]
+      @printf f_conjugated "%f,"  lattice_QH[2,idx]
+      @printf f_conjugated "%f,"  0.0
+      @printf f_conjugated "%f,"  sx_expval_c[idx]
+      @printf f_conjugated "%f,"  sy_expval_c[idx]
+      @printf f_conjugated "%f,"  sz_expval_c[idx]
+      @printf f_conjugated "%f\n" sqrt(sx_expval_c[idx]^2 + sy_expval_c[idx]^2 + sz_expval_c[idx]^2)
+    end  
+    close(f_conjugated)
+    println("For alpha = $α: Final energy of psi conjugated = $E_c")
+    println("For alpha = $α: Final energy variance of psi conjugated = $σ_c")
+
+    psi_file = h5open("$(formatted_alpha)_Mag2D_original.h5","w")
+    write(psi_file,"Psi",ψ)
+    close(psi_file)
+
+    psi_file_conj = h5open("$(formatted_alpha)_Mag2D_conjugated.h5","w")
+    write(psi_file_conj,"Psi_c",ψ_c)
+    close(psi_file_conj)
+
+    push!(Energies, (α, norm(E), norm(E_c), norm(σ), norm(σ_c))) 
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection = "3d")
+    
+    for idx in axes(lattice_QH,2)
+        t = sz_expval_c
+        x, y, z = lattice_QH[1,idx],lattice_QH[2,idx], 0.0
+        vmin = minimum(t)
+        vmax = maximum(t)
+        cmap = PyPlot.matplotlib.cm.get_cmap("rainbow_r") 
+        norm = PyPlot.matplotlib.colors.Normalize(vmin=vmin,vmax=vmax)
+        ax.quiver(x, y, z, sx_expval_c[idx], sy_expval_c[idx], sz_expval_c[idx], normalize=true, color=cmap(norm(t[idx])))
+        plt.xlabel("x")
+        plt.ylabel("y")
+    end
+    ax.set_aspect("equal")
+    plt.show()
+    Q = calculate_TopoCharge(sx_expval_c, sy_expval_c, sz_expval_c, lattice_QH, Lx, Ly)
+    @show Q
+    
+    return
+end
+
+
     # ITensors.disable_warn_order()
     # full_H = 1.0
     # ctr = 0
@@ -285,6 +389,3 @@ let
     # full_H_arr = array(full_H_ordered)
     # shape = size(full_H_arr)
     # full_H_matrix = sparse(reshape(full_H_arr, (prod(shape[1:length(shape)÷2]), prod(shape[length(shape)÷2+1:end]))))
-
-    return
-end
