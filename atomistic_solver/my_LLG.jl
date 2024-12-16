684d7f0a990366c7526be6ac264f6a7f9bb6673f
steli42 <liscak@gmail.com>
2024-12-16 12:31:47 +0100

    not great not terrible llg

--- atomistic_solver/my_LLG.jl
+++ atomistic_solver/my_LLG.jl
@@ -73,49 +73,83 @@ function initialize_m(case::String, lattice::Matrix{Float64}, D, α, w, R, ecc)
         m = rotate_m(m, θϕ)
     end
 
+    # Lx,Ly = 15,15
+    # idx = Lx*Ly ÷ 2
+    # m[idx] = [0,0,-1]
     @info "Initialized"
     return m
 end
 
-function calculate_energy(m::Any, lattice::Matrix{Float64}, nn_idxs::Vector{Vector{Int64}}, B, J, D, α, alpha_axis::Int64)
-    energy = 0.0
+function calculate_energy(m::Any, lattice::Matrix{Float64}, boundary::Matrix{Float64},
+    nn_idxs_LL::Vector{Vector{Int64}}, nn_idxs_LB::Vector{Vector{Int64}}, 
+    B, Bg, J, D, ms, α, alpha_axis::Int64)
 
+    # axis = alpha_axis == 1 ? 2 : 1
+    energy = 0.0
+    
+    # intra-lattice pairs
     for idx in axes(lattice,2)
         
         # zeeman
         energy += dot(B, m[idx])
-
+        # gradient perturbation
+        energy += dot(lattice[2,idx] * Bg, m[idx])
+        
         # pair-wise interactions
-        for nn_idx in nn_idxs[idx]
-
+        for nn_idx in nn_idxs_LL[idx]
+            
             # exchange
             energy += 0.5 * J * dot(m[idx], m[nn_idx]) 
-
+            
             # construct dmi vector
             r_rel = lattice[:,nn_idx] - lattice[:,idx]
             r_rel_3D = normalize(vcat(r_rel,0))
             D_vector = D * r_rel_3D
             D_vector[alpha_axis] *= α
+            
+            # dmi
+            m_cross = cross(m[idx], m[nn_idx]) 
+            energy += 0.5 * dot(D_vector, m_cross)
+        end
+    end
+    
+    # lattice-boundary interactions
+    mb = -ms * normalize(B) 
+    for idx in axes(boundary,2)
+        for nn_idx in nn_idxs_LB[idx]
+
+            # exchange
+            energy += J * dot(mb, m[nn_idx]) 
+
+            # construct dmi vector
+            r_rel = lattice[:,nn_idx] - boundary[:,idx]
+            r_rel_3D = normalize(vcat(r_rel,0))
+            D_vector = D * r_rel_3D
+            D_vector[alpha_axis] *= α
 
             # dmi
-            m_cross = cross(m[idx], m[nn_idx]) # havent checked that this is the correct order so if theres and extra sign in D, look here
-            energy += 0.5 * dot(D_vector, m_cross)
+            m_cross = cross(m[nn_idx], mb) 
+            energy += dot(D_vector, m_cross)
         end
     end
 
     return energy
 end
 
-function calculate_Heff(m::Any, lattice::Matrix{Float64}, nn_idxs::Vector{Vector{Int64}}, B, J, D, α, alpha_axis::Int64)
+function calculate_Heff(m::Any, lattice::Matrix{Float64}, boundary::Matrix{Float64},
+    nn_idxs_LL::Vector{Vector{Int64}}, nn_idxs_LB::Vector{Vector{Int64}},
+    B, Bg, J, D, ms, α, alpha_axis::Int64)
     Heff = [SVector{3, Float64}(0.0, 0.0, 0.0) for _ in 1:length(m)]
+    # axis = alpha_axis == 1 ? 2 : 1
     
+    # H_eff from internal site neighbors
     for idx in axes(lattice,2)
         
         # zeeman
         Heff[idx] += B
         
         # pair-wise interactions
-        for nn_idx in nn_idxs[idx]
+        for nn_idx in nn_idxs_LL[idx]
 
             # exchange
             Heff[idx] += J * m[nn_idx]  
@@ -131,17 +165,44 @@ function calculate_Heff(m::Any, lattice::Matrix{Float64}, nn_idxs::Vector{Vector
         end
     end
 
+    # H_eff from boundary sites
+    mb = -ms * normalize(B) 
+    for idx in axes(boundary,2)
+        for nn_idx in nn_idxs_LB[idx]
+
+            # exchange
+            Heff[nn_idx] += J * mb 
+            
+            # construct dmi vector
+            r_rel = lattice[:,nn_idx] - boundary[:,idx]
+            r_rel_3D = normalize(vcat(r_rel,0))
+            D_vector = D * r_rel_3D
+            D_vector[alpha_axis] *= α
+
+            # dmi
+            Heff[nn_idx] += cross(D_vector, mb) 
+        end
+    end
+
+    # gradient perturbation
+    for idx in axes(lattice,2)
+        Heff[idx] += lattice[2,idx] * Bg
+    end
+
     return Heff
 end
 
-function llg_solver_rk4(m::Any, lattice::Matrix{Float64}, nn_idxs::Vector{Vector{Int64}}, B, J, D, α, alpha_axis::Int64, Γ, τ, dt, steps)
+function llg_solver_rk4(m::Any, lattice::Matrix{Float64}, boundary::Matrix{Float64},
+    nn_idxs_LL::Vector{Vector{Int64}}, nn_idxs_LB::Vector{Vector{Int64}},
+    B, Bg, J, D, ms, α, alpha_axis::Int64, 
+    Γ, τ, dt, steps, relax::Bool)
 
     function compute_dmdt(m::Any, H_eff::Any)
         dm_dt = [SVector{3, Float64}(0.0, 0.0, 0.0) for _ in eachindex(m)]
         for idx in eachindex(m)
             precession_term =  cross(m[idx], H_eff[idx])
             damping_term =  cross(m[idx], precession_term)
-            dm_dt[idx] = - Γ * precession_term + τ * damping_term
+            dm_dt[idx] = Γ * precession_term + τ * damping_term
         end
         return dm_dt
     end
@@ -154,22 +215,21 @@ function llg_solver_rk4(m::Any, lattice::Matrix{Float64}, nn_idxs::Vector{Vector
     ε = 1e-8
     ctr = 1
     for step in pbar
-        H_eff_1 = calculate_Heff(m, lattice, nn_idxs, B, J, D, α, alpha_axis)
-        k1 = compute_dmdt(m, H_eff_1) # dt * k1 is a small increment dm
+        H_eff_1 = calculate_Heff(m, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
+        k1 = compute_dmdt(m, H_eff_1) 
 
         m_temp = [normalize(m[idx] + 0.5 * dt * k1[idx]) for idx in eachindex(m)]
-        H_eff_2 = calculate_Heff(m_temp, lattice, nn_idxs, B, J, D, α, alpha_axis)
+        H_eff_2 = calculate_Heff(m_temp, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
         k2 = compute_dmdt(m_temp, H_eff_2) 
 
         m_temp = [normalize(m[idx] + 0.5 * dt * k2[idx]) for idx in eachindex(m)]
-        H_eff_3 = calculate_Heff(m_temp, lattice, nn_idxs, B, J, D, α, alpha_axis)
+        H_eff_3 = calculate_Heff(m_temp, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
         k3 = compute_dmdt(m_temp, H_eff_3)
 
         m_temp = [normalize(m[idx] + dt * k3[idx]) for idx in eachindex(m)]
-        H_eff_4 = calculate_Heff(m_temp, lattice, nn_idxs, B, J, D, α, alpha_axis)
+        H_eff_4 = calculate_Heff(m_temp, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
         k4 = compute_dmdt(m_temp, H_eff_4)
         
-
         for idx in eachindex(m)
             m[idx] += (dt / 6.0) * (k1[idx] + 2.0 * k2[idx] + 2.0 * k3[idx] + k4[idx])
             m[idx] = normalize(m[idx])  
@@ -181,39 +241,34 @@ function llg_solver_rk4(m::Any, lattice::Matrix{Float64}, nn_idxs::Vector{Vector
         torque_diff = abs(current_avg_torque - prevs_avg_torque)
         prevs_avg_torque = current_avg_torque
 
-        # Check convergence
-        if torque_diff < ε && step > 1000
+        ctr += 1
+        # Check convergence if relax is true
+        if torque_diff < ε && step > 1000 && relax
             @info "Converged at step $step with torque_diff = $torque_diff"
             break
         end
-        ctr += 1
     end
 
     return m_evol, ctr
 end
 
-function average_m_proj(m_evol::Any, time_step::Int64, proj::String)
+function average_m_proj(m::Any, proj::String)
     if proj == "mx"
-        magnetization_at_t = m_evol[time_step]
-        average_m = mean(mag[1] for mag in magnetization_at_t)
+        average_m = mean(mag[1] for mag in m)
     elseif proj == "my"
-        magnetization_at_t = m_evol[time_step]
-        average_m = mean(mag[2] for mag in magnetization_at_t)   
+        average_m = mean(mag[2] for mag in m)   
     elseif proj == "mz"
-        magnetization_at_t = m_evol[time_step]
-        average_m = mean(mag[3] for mag in magnetization_at_t)  
+        average_m = mean(mag[3] for mag in m)  
     end
 
     return average_m
 end
 
-function calculate_avg_torque(m::Vector{SVector{3, Float64}}, Heff::Vector{SVector{3, Float64}})
+function calculate_avg_torque(m::Any, Heff::Vector{SVector{3, Float64}})
     T = [SVector{3, Float64}(0.0, 0.0, 0.0) for _ in 1:length(m)]
     
     for i in eachindex(m)
-        # Compute torque: T_i = something * M_i × H_eff_i - something * M_i × (M_i × H_eff_i)
         precession = cross(m[i], Heff[i])
-        # damping = cross(m[i], cross(m[i], Heff[i]))
         T[i] = precession 
     end
     
@@ -221,72 +276,14 @@ function calculate_avg_torque(m::Vector{SVector{3, Float64}}, Heff::Vector{SVect
     return avg_torque_magnitude
 end
 
-let 
-    Lx,Ly = 20,20
-    Ld = 12
-    case = "FM" # "rand"/"FM"/"SK"
-    J = -1.0 
-    D =  -2*π/Ld 
-    Bamp = -0.65 * D^2  
-    B = Bamp * [0.0, 0.0, 1.0]
-    α = 1.0
-    alpha_axis = 1
-    w = 1.5 
-    R = 4.5 
-    ecc = 1.0
-
-    ms = 1.0
-    β = 1.0
-    Γ = 1.0 / (1 + β^2)        
-    τ = β / (1 + β^2)
-    dt = 1e-2
-    steps = Int(1e4)
-
-    lattice = construct_lattice(Lx,Ly)
-    tree = KDTree(lattice, reorder=false)
-
-    onsite_idxs = inrange(tree, tree.data, 0.01)
-    aux_idxs = inrange(tree, tree.data, 1.01)
-    nn_idxs = setdiff.(aux_idxs, onsite_idxs) # array of arrays; elements of array A are idxs of nearest neighbors of moment A
-
-    # remember for later that the norm of magnetization should be 1/2 in the end
-    m = initialize_m(case, lattice, D, α, w, R, ecc) # array of arrays; rows label moments and columns projections
-    m_FM = initialize_m("FM", lattice, D, α, w, R, ecc) 
-    energy = calculate_energy(m, lattice, nn_idxs, B, J, D, α, alpha_axis) 
-    println("Initial state energy is: $energy")
-    
-    m_evol, ctr = llg_solver_rk4(m, lattice, nn_idxs, B, J, D, α, alpha_axis, Γ, τ, dt, steps)  # m_evol[i] is the configuration at (i-1)-th time step 
-    energy = calculate_energy(m_evol[ctr+1], lattice, nn_idxs, B, J, D, α, alpha_axis) 
-    println("Final time-evolved state energy is: $energy")
-    time = 1:ctr+1
-    
-    # m = m_evol[1]
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection = "3d")
-    # t = [m[i][3] for i in axes(m,1)]
-    # for idx in axes(lattice,2)
-    #     x, y, z = lattice[1,idx],lattice[2,idx], 0.0
-    #     mx, my, mz = m[idx][1],m[idx][2],m[idx][3]
-    #     vmin = -ms 
-    #     vmax = ms
-    #     cmap = PyPlot.matplotlib.cm.get_cmap("RdBu_r") 
-    #     norm = PyPlot.matplotlib.colors.Normalize(vmin=vmin,vmax=vmax)
-    #     ax.quiver(x, y, z, mx, my, mz, normalize=true, color=cmap(norm(t[idx])))
-    #     plt.xlabel("x")
-    #     plt.ylabel("y")
-    # end
-    # ax.set_aspect("equal")
-    # plt.show()
-    
-
-    m = m_evol[ctr+1]
+function plot_m_texture(m::Any, lattice::Matrix{Float64}, ms)
     fig = plt.figure()
     ax = fig.add_subplot(projection = "3d")
-    t = [m[i][3] for i in axes(m,1)] 
+    t = [m[i][3] for i in axes(m,1)]
     for idx in axes(lattice,2)
         x, y, z = lattice[1,idx],lattice[2,idx], 0.0
         mx, my, mz = m[idx][1],m[idx][2],m[idx][3]
-        vmin = -ms
+        vmin = -ms 
         vmax = ms
         cmap = PyPlot.matplotlib.cm.get_cmap("RdBu_r") 
         norm = PyPlot.matplotlib.colors.Normalize(vmin=vmin,vmax=vmax)
@@ -296,11 +293,29 @@ let
     end
     ax.set_aspect("equal")
     plt.show()
-    
+end
+
+function plot_avg_torque(m_evol::Any, lattice::Matrix{Float64}, boundary::Matrix{Float64},
+    nn_idxs_LL::Vector{Vector{Int64}}, nn_idxs_LB::Vector{Vector{Int64}}, time, B, Bg, 
+    J, D, ms, α, alpha_axis::Int64)
+
     fig = plt.figure()
-    mx_of_t = [average_m_proj(m_evol, t, "mx") for t in time]
-    my_of_t = [average_m_proj(m_evol, t, "my") for t in time]
-    mz_of_t = [average_m_proj(m_evol, t, "mz") for t in time]
+    T_avg = [calculate_avg_torque(m_evol[t], calculate_Heff(m_evol[t], lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)) for t in time]
+    plot(time, T_avg, color="black", label="T")
+    xlabel("t")
+    ylabel("Torque")
+    legend()
+    grid(true)
+    show()
+    plt.savefig("atomistic_solver/torque.png",dpi=600)
+
+end
+
+function plot_avg_m_projections(m_evol::Any, time)
+    fig = plt.figure()
+    mx_of_t = [average_m_proj(m_evol[t], "mx") for t in time]
+    my_of_t = [average_m_proj(m_evol[t], "my") for t in time]
+    mz_of_t = [average_m_proj(m_evol[t], "mz") for t in time]
     plot(time, mx_of_t, color="red", label="m_x(t)")
     plot(time, my_of_t, color="blue", label="m_y(t)")
     plot(time, mz_of_t, color="green", label="m_z(t)")
@@ -308,17 +323,146 @@ let
     ylabel("Average m")
     legend()
     grid(true)
-    plt.show()
-
-    fig = plt.figure()
-    T_avg = [calculate_avg_torque(m_evol[t], calculate_Heff(m_evol[t], lattice, nn_idxs, B, J, D, α, alpha_axis)) for t in time]
-    plot(time, T_avg, color="black", label="T")
-    xlabel("t")
-    ylabel("Torque")
-    legend()
-    grid(true)
     show()
-    plt.show()
+    plt.savefig("atomistic_solver/magnetisations.png",dpi=600)
+end
+
+function calculate_rs(m_evol::Any, lattice::Matrix{Float64}, time, ms)
+
+    x_avg = zeros(length(time))
+    y_avg = zeros(length(time))
+    x_aux(t) = sum(lattice[1,idx]*(mag[3] - ms) for (idx,mag) in enumerate(m_evol[t])) 
+    y_aux(t) = sum(lattice[2,idx]*(mag[3] - ms) for (idx,mag) in enumerate(m_evol[t])) 
+    Ns(t) = sum((mag[3] - ms) for mag in m_evol[t]) 
+    for t in time
+        N = Ns(t)
+        x_avg[t] = x_aux(t)/N
+        y_avg[t] = y_aux(t)/N
+    end   
+    
+    return x_avg, y_avg
+end
+    
+
+let 
+    Lx,Ly = 31,15
+    Ld = 12
+    case = "SK" # "rand"/"FM"/"SK"
+    ms = 1/2  
+    J = -1.0 
+    # D = J*2*π/Ld   
+    D = 0.5*J
+    # B = 1.2 * J * 0.5^2 * D^2 * [0.0, 0.0, 1.0] /ms  
+    B = [0.0, 0.0, 0.075*J]/ms
+    α = 1.0
+    alpha_axis = 1
+    w = 1.5 
+    R = 4.5 
+    ecc = 1.0
+    
+    μ = -1.0  
+    β = 1.0
+    Γ = -μ*ms / (1 + (β*ms)^2)        
+    τ = -μ * β * ms^2 / (1 + (β*ms)^2) 
+    dt = 1e-1
+    steps = Int(1e4) #the minimum is 1e3 time steps
+
+    lattice = construct_lattice(Lx,Ly)
+    boundary = construct_lattice(Lx+2,Ly+2)
+    idxs_LB = []
+    for iB in axes(boundary,2)
+        for iL in axes(lattice,2)
+            if boundary[:, iB] == lattice[:, iL]
+                push!(idxs_LB, iB)
+            end
+        end
+    end
+    boundary = boundary[:,setdiff(1:size(boundary,2),idxs_LB)]
+
+    tree_L = KDTree(lattice, reorder=false)
+    tree_B = KDTree(boundary, reorder=false)
+
+    onsite_idxs = inrange(tree_L, tree_L.data, 0.01)
+    aux_idxs = inrange(tree_L, tree_L.data, 1.01)
+    nn_idxs_LL = setdiff.(aux_idxs, onsite_idxs) 
+    nn_idxs_LB = inrange(tree_L, tree_B.data, 1.01)
+
+    
+    ##RELAXATION BLOCK############################################################################### 
+    Bg = zeros(3)
+    m = initialize_m(case, lattice, D, α, w, R, ecc) # array of arrays; rows label site moments and columns projections
+    vacuum = [normalize(SVector(0.0, 0.0, 1.0)) for _ in 1:size(lattice,2)] 
+    energy = calculate_energy(m, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis) - 
+        calculate_energy(vacuum, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
+    println("Relative energy of initial state is: $energy")
+    
+    # m_evol[i] is the configuration at (i-1)-th time step
+    m_evol, ctr = llg_solver_rk4(m, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis, Γ, τ, dt, steps, true)   
+    energy = calculate_energy(m_evol[ctr], lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis) - 
+        calculate_energy(vacuum, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
+    println("Relative energy of relaxed state is: $energy")
+    time = 1:ctr
+
+    # plot_m_texture(m_evol[1],lattice,ms) # plots initial texture
+    # plot_m_texture(m_evol[ctr],lattice,ms) # plots final texture
+    # plot_avg_m_projections(m_evol,time)    # plots m projection as functions of time
+    # plot_avg_torque(m_evol, lattice, boundary, nn_idxs_LL, nn_idxs_LB, time, B, Bg, J, D, ms, α, alpha_axis) # plots average torque as a function of time
+
+    # ################################################################################################# 
+    # ################################################################################################# 
+
+    β = 0.0
+    Γ = -μ*ms / (1 + (β*ms)^2)        
+    τ = -μ * β * ms^2 / (1 + (β*ms)^2) 
+    dt = 0.001
+    steps = 25*Int(1e3) 
+    # Bg = -2 * 0.02 / Ly * [0.0, 0.0, 1.0] * D^2/ms 
+    Bg = [0.0, 0.0, -0.02] / ms
+
+    @show B 
+    @show Bg
+
+    m = m_evol[ctr]
+    m_evol2, ctr = llg_solver_rk4(m, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis, Γ, τ, dt, steps, false)  
+    energy = calculate_energy(m_evol2[ctr], lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis) - 
+        calculate_energy(vacuum, lattice, boundary, nn_idxs_LL, nn_idxs_LB, B, Bg, J, D, ms, α, alpha_axis)
+    println("energy of final time-evolved state is: $energy")
+    time = 1:ctr
+
+    plot_m_texture(m_evol2[1],lattice,ms) # plots initial texture
+    plot_m_texture(m_evol2[ctr],lattice,ms) # plots final texture
+    # plot_avg_m_projections(m_evol2,time)    # plots m projection as functions of time
+    # plot_avg_torque(m_evol2, lattice, boundary, nn_idxs_LL, nn_idxs_LB, time, B, Bg, J, D, ms, α, alpha_axis) # plots average torque as a function of time
+
+    x_com, y_com = calculate_rs(m_evol2, lattice, time, ms)
+
+    plt.figure()
+    plot(dt*time,x_com)
+    ylabel("x(t)")
+    plt.figure()
+    plot(dt*time,y_com)
+    ylabel("y(t)")
+
+    # plt.figure()
+    # N = zeros(length(time))
+    # Ns(t) = sum((mag[3] - ms) for mag in m_evol2[t]) 
+    # for (idx,t) in enumerate(time)
+    #     N[idx] = Ns(t)
+    # end
+    # plot(dt*time,N)
+    # ylabel("Ns(t)")
+
+
+
+
+
+
+
+
+
+
+
+
 
     return
 end
@@ -326,27 +470,11 @@ end
     # For plotting the lattice  ############################################
 
     # plt.scatter(lattice[1,:], lattice[2,:])
+    # plt.scatter(boundary[1,:], boundary[2,:])
     # for id in axes(lattice, 2)
     #     plt.text(lattice[1,id], lattice[2,id], "$id")
     # end
-    # plt.show()
-
-    # For plotting the magnetization #######################################
-
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection = "3d")
-        
-    # for idx in axes(lattice,2)
-    #     t = [m[i][3] for i in axes(m,1)]
-    #     x, y, z = lattice[1,idx],lattice[2,idx], 0.0
-    #     mx, my, mz = m[idx][1],m[idx][2],m[idx][3]
-    #     vmin = minimum(t)
-    #     vmax = maximum(t)
-    #     cmap = PyPlot.matplotlib.cm.get_cmap("rainbow_r") 
-    #     norm = PyPlot.matplotlib.colors.Normalize(vmin=vmin,vmax=vmax)
-    #     ax.quiver(x, y, z, mx, my, mz, normalize=false, color=cmap(norm(t[idx])))
-    #     plt.xlabel("x")
-    #     plt.ylabel("y")
+    # for id in axes(boundary, 2)
+    #     plt.text(boundary[1,id], boundary[2,id], "$id")
     # end
-    # ax.set_aspect("equal")
     # plt.show()
